03719f7bb80dd882b33e98bfe9cd5eafb74b2de7
Radosław Mieckowski <rmieckowski@wp.pl>
2023-01-23 00:07:16 +0100

    add FrogRiverOne.java

---
+++ src/main/java/FrogRiverOne.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+// https://app.codility.com/programmers/lessons/4-counting_elements/
+public class FrogRiverOne {
+    public int perfectSolution(int X, int[] A) {
+        Set<Integer> displacementSet = new HashSet<>();
+        for (int i = 0; i < A.length; i++) {
+            displacementSet.add(A[i]);
+            if (displacementSet.size() == X) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
