b88062db1179f980aacf4910eface78f8aace808
Radosław Mieckowski <rmieckowski@wp.pl>
2023-01-23 23:32:30 +0100

    add PermCheck.java

---
+++ src/main/java/PermCheck.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+//Check whether array A is a permutation.
+public class PermCheck {
+    public int perfectSolution(int[] A) { // works even if the min value is not 1, for min=1 u can use A.length instead of max
+        Set<Integer> elements = new HashSet<>();
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < A.length; i++) {
+            if (elements.contains(A[i])) {
+                return 0;
+            }
+            elements.add(A[i]);
+            if (A[i] > max) {
+                max = A[i];
+            }
+        }
+        if (max == A.length) {
+            return 1;
+        }
+        return 0;
+    }
+}
